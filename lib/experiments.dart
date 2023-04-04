6d28938f4ae295a1b4993dc5d20cb2c1ebd5bbbb
Samriddha <samriddha97@gmail.com>
2023-04-04 22:03:42 +0530

    null safety in dart

---
+++ lib/experiments.dart
@@ -0,0 +1,80 @@
+class Experiments {
+  void funIfElseStatements() {
+    var name = ['owl', 'fowl', 'houl', 'cowl', 'lowl'];
+    name.add('owl');
+    final myName = "sam";
+    for (var item in name) {
+      if (item == myName) {
+        print("Your Name is found");
+      } else {
+        print("Your Name is not found in the above variable");
+      }
+    }
+  }
+
+  void operatorsInDart() {
+    // Infix operators are *, +, -,/ etc.
+    final age = 54;
+    var multiplyAge = age * 2;
+    var devideAge = age / 2;
+    var agePlusTen = age + 10;
+    print('Original age is:$age');
+    print('Original age*2 is:$multiplyAge');
+    print('Original age/2 is:$devideAge');
+    print('Original age+10 is:$agePlusTen');
+
+    //Prefix & postfix operators : -- ++
+    var number = 99;
+    print('Number -- is :${--number}');
+    print('Number ++ is :${++number},${number++}');
+    print('Number is :${number--}');
+    print('Number is :${number}');
+  }
+
+  void listsInDart() {
+    final listOfNumbers = [1, 2, 3, 4, 5, 6];
+    for (var itr = 0; itr < listOfNumbers.length; itr++) {
+      print('Index : $itr | Item in the index is : ${listOfNumbers[itr]}');
+    }
+  }
+
+  void setInDart() {
+    var name = {'owl', 'fowl', 'houl', 'cowl', 'lowl'};
+    name.add('owl');
+    name.forEach((element) {
+      print(element);
+    });
+    // We will see that the 'owl' element will be prited only once
+    // because set doesn't allow us to save two same elements inside it.
+  }
+
+  void mapsInDart() {
+    var myMap = {'key_1': 1223};
+    myMap['key_2'] = 0021;
+    myMap['key_3'] = 02144;
+    myMap['key_4'] = 1908;
+    for (var key in myMap.keys) {
+      print('Key Name : $key value : ${myMap[key]}');
+    }
+  }
+
+  void nullSefetyInDart(String? firstName, String? lastName) {
+    String? myValue =
+        null; // to declear any variable that can hold a null value.
+    print(myValue);
+    myValue = 'ooasdbasda';
+    print(myValue);
+
+    // CherryPicking Not Null : ?? is like elvis operator(:?) in java it will chekck if the first name
+    // is null then it will return last name. If the first name is not null then it
+    // will just return the firstname;
+    print(firstName ?? lastName ?? 'both are null');
+
+    // Null aware assigment operator : ??=
+    // this operator will check for a null value and if there is a null value
+    // then it will assign a new value to it.
+    print(firstName ??= 'new value assigned');
+  }
+
+  List<String>? myList = null;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:learn_dart/experiments.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-void funIfElseStatements() {
-  var name = ['owl', 'fowl', 'houl', 'cowl', 'lowl'];
-  name.add('owl');
-  final myName = "sam";
-  for (var item in name) {
-    if (item == myName) {
-      print("Your Name is found");
-    } else {
-      print("Your Name is not found in the above variable");
-    }
-  }
-}
-
-void operatorsInDart() {
-  // Infix operators are *, +, -,/ etc.
-  final age = 54;
-  var multiplyAge = age * 2;
-  var devideAge = age / 2;
-  var agePlusTen = age + 10;
-  print('Original age is:$age');
-  print('Original age*2 is:$multiplyAge');
-  print('Original age/2 is:$devideAge');
-  print('Original age+10 is:$agePlusTen');
-
-  //Prefix & postfix operators : -- ++
-  var number = 99;
-  print('Number -- is :${--number}');
-  print('Number ++ is :${++number},${number++}');
-  print('Number is :${number--}');
-  print('Number is :${number}');
-}
-
-void listsInDart() {
-  final listOfNumbers = [1, 2, 3, 4, 5, 6];
-  for (var itr = 0; itr < listOfNumbers.length; itr++) {
-    print('Index : $itr | Item in the index is : ${listOfNumbers[itr]}');
-  }
-}
-
-void setInDart() {
-  var name = {'owl', 'fowl', 'houl', 'cowl', 'lowl'};
-  name.add('owl');
-  name.forEach((element) {
-    print(element);
-  });
-  // We will see that the 'owl' element will be prited only once
-  // because set doesn't allow us to save two same elements inside it.
-}
-
-void mapsInDart() {
-  var myMap = {'key_1': 1223};
-  myMap['key_2'] = 0021;
-  myMap['key_3'] = 02144;
-  myMap['key_4'] = 1908;
-  for (var key in myMap.keys) {
-    print('Key Name : $key value : ${myMap[key]}');
-  }
 }
 
 class MyApp extends StatelessWidget {
@@ -69,11 +11,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    //testFunction();
-    //operatorsInDart();
-    //listsInDart();
-    //setInDart();
-    mapsInDart();
+    var myObject = Experiments();
+    myObject.nullSefetyInDart(null, null);
+    print(myObject.myList?.length ?? 'size is 0');
     return MaterialApp(
       title: 'Sam Demo',
       theme: ThemeData(
@@ -137,7 +77,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
-        backgroundColor: Colors.yellowAccent,
+        backgroundColor: Colors.greenAccent,
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
